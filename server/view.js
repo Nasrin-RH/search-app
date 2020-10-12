01052186fdaf691a7421820fc8a7db656e682046
nasrin hussain <nasrinhussain2017@gmail.com>
2020-10-12 17:19:56 +0530

    correct code

--- server/view.js
+++ server/view.js
@@ -6,39 +6,39 @@ module.exports = function (request, response) {
     WHERE
      name iLIKE '` + request.params.search_text + `%' 
      OR
-    company iLIKE '%` + request.params.search_text + `%'
+    company iLIKE '` + request.params.search_text + `%'
      OR
-     department iLIKE '%` + request.params.search_text + `%'
+     department iLIKE '` + request.params.search_text + `%'
      OR 
-     designation iLIKE '%` + request.params.search_text + `%'
+     designation iLIKE '` + request.params.search_text + `%'
      OR 
-     email iLIKE '%` + request.params.search_text + `%'
+     email iLIKE '` + request.params.search_text + `%'
      OR 
      contact::text iLIKE '` + request.params.search_text + `%'
      OR 
-     id::text iLIKE '` + request.params.search_text + `%' 
-     union all 
-     SELECT dept_id,dept_name,service,user_id 
-     FROM department 
-     WHERE 
-     dept_name iLIKE '` + request.params.search_text + `%' 
-     OR 
-     service iLIKE '%` + request.params.search_text + `%'
-     OR 
-     dept_id::text iLIKE '` + request.params.search_text + `%'
-     OR 
-     user_id::text iLIKE '` + request.params.search_text + `%'
-     union all 
-     SELECT cust_id,cust_name,service,user_id 
-     FROM customer 
-     WHERE 
-     cust_name iLIKE '` + request.params.search_text + `%' 
-     OR 
-     service iLIKE '%` + request.params.search_text + `%'
-     OR 
-     cust_id::text iLIKE '%`+ request.params.search_text + `%'
-     OR 
-     user_id::text iLIKE  '%`+ request.params.search_text+ `%'`;
+     id::text iLIKE '` + request.params.search_text + `%' `;
+    //  union all 
+    //  SELECT dept_id,dept_name,service,user_id 
+    //  FROM department 
+    //  WHERE 
+    //  dept_name iLIKE '` + request.params.search_text + `%' 
+    //  OR 
+    //  service iLIKE '%` + request.params.search_text + `%'
+    //  OR 
+    //  dept_id::text iLIKE '` + request.params.search_text + `%'
+    //  OR 
+    //  user_id::text iLIKE '` + request.params.search_text + `%'
+    //  union all 
+    //  SELECT cust_id,cust_name,service,user_id 
+    //  FROM customer 
+    //  WHERE 
+    //  cust_name iLIKE '` + request.params.search_text + `%' 
+    //  OR 
+    //  service iLIKE '%` + request.params.search_text + `%'
+    //  OR 
+    //  cust_id::text iLIKE '%`+ request.params.search_text + `%'
+    //  OR 
+    //  user_id::text iLIKE  '%`+ request.params.search_text+ `%'`;
     console.log(q);
     pool.query(q, (error, results) => {
         if(error) {
